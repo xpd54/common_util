d3e219efbc14ea4d967d6eb61f7060b3076b6ad4
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-02-01 21:20:45 +0800

    set precision on float

--- include/common_util/string_format_util.hpp
+++ include/common_util/string_format_util.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include <iomanip>
 #include <sstream>
 #include <string>
 
 namespace common_util {
 template <typename... Args> std::string string_format(Args... args) {
   std::ostringstream stream;
+  stream << std::fixed << std::setprecision(4);
   (stream << ... << args);
   return stream.str();
 }
